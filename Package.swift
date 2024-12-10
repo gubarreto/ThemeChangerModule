66b5fddbb7457e2352b4dd4a7684e0842c2c8f36
Gustavo Barreto <gustavobarretodaf@gmail.com>
2024-12-10 13:46:26 -0300

    Update minimal iOS version to 17

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ThemeChangerModule",
     platforms: [
-        .iOS(.v16),
+        .iOS(.v17),
         .macOS(.v14)
     ],
     products: [
